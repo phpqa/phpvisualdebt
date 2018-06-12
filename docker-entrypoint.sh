37be2ebb5dd75877c82bfd97867cf568152dbb1c
MetalArend <metalarend@gmail.com>
2018-06-12 23:21:36 +0200

    remove the command, add directory check

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,27 +1,13 @@
 #!/bin/sh
 set -e
 
-isCommand() {
-  for cmd in \
-    "analyse" \
-    "help" \
-    "list"
-  do
-    if [ -z "${cmd#"$1"}" ]; then
-      return 0
-    fi
-  done
-
-  return 1
-}
-
 if [ "${1:0:1}" = "-" ]; then
   set -- /sbin/tini -- php /vendor/bin/phpvisualdebt "$@"
 elif [ "$1" = "/vendor/bin/phpvisualdebt" ]; then
   set -- /sbin/tini -- php "$@"
 elif [ "$1" = "phpvisualdebt" ]; then
   set -- /sbin/tini -- php /vendor/bin/"$@"
-elif isCommand "$1"; then
+elif [ -d "$1" ]; then
   set -- /sbin/tini -- php /vendor/bin/phpvisualdebt "$@"
 fi
 
